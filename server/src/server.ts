32dbe5e8b7545cacce190253a0de9dd5bd09d6db
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-09-14 01:33:56 -0300

    finaliza backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,96 @@
 import express from 'express';
 import cors from "cors";
+import { PrismaClient } from  '@prisma/client';
+import { convertHourstringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutestoHourString } from './utils/convert-minutes-to-hour-string';
 
 const app = express();
 app.use(cors());
+app.use(express.json())
+const prisma = new PrismaClient(
+    {
+        log: ['query']
+    }
+) // faz a conexão com o banco de dados
 
-app.get("/ads", (req, res) => {
+app.get("/games",  async (req, res) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true,
+                }
+            }
+        }
+    })
+    return res.json(games);
+});
+app.post("/games/:id/ads", async (req, res) => {
+    const gameId = req.params.id;
+    const body = req.body;
+    const ad = await prisma.ad.create({
+        data:{
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: convertHourstringToMinutes(body.hourStart),
+            hourEnd: convertHourstringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+    return res.status(201).json(ad);
+});
+app.get("/games/:id/ads", async (req, res) => {
+   const gameId = req.params.id
+   const ads = await prisma.ad.findMany({
+       select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+       },
+       where: {
+           gameId,
+       },
+       orderBy: {
+           createdAt: 'desc',
+       }
+   })
+    return res.json(ads.map(ad => {
+        return {
+            ...ad, 
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutestoHourString(ad.hourStart),
+            hourEnd: convertMinutestoHourString(ad.hourEnd)
 
-    return res.json([
-        { id: 1, nome: 'Anúncio 1'},
-        { id: 2, nome: 'Anúncio 2'},
-        { id: 3, nome: 'Anúncio 3'}
-    ])
-})
+        }
+    }))
+});
+app.get("/ads/:id/discord", async (req, res) => {
+    const adId = req.params.id;
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+        return res.json({
+            discord: ad.discord,
+        })
+});
 
 app.listen(3002, () => {
     console.log('Servidor rodando na porta 3002')
 })
+/* 
+Adicionar validação
+zod js
+UntilTheEnd
+*/
